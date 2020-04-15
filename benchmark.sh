e0172cbaa3522befe350e477f31768ba04327a32
ankan2013 <45983282+ankan2013@users.noreply.github.com>
2020-04-15 15:44:48 +0300

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,16 +1,16 @@
 mkdir benchmark_results
-#sudo yum -y install wget
-#sudo yum -y install fio
-#sudo yum -y install tar
+sudo yum -y install wget
+sudo yum -y install fio
+sudo yum -y install tar
 
-#sudo chmod +x benchmark_scripts/miniowarp.sh
-#sudo chmod +x benchmark_scripts/fio_rbd.sh
-#sudo chmod +x benchmark_scripts/gobench.sh
-#sudo chmod +x benchmark_scripts/fio_cephfs.sh
-#sudo chmod +x benchmark_scripts/rados.sh
+sudo chmod +x benchmark_scripts/miniowarp.sh
+sudo chmod +x benchmark_scripts/fio_rbd.sh
+sudo chmod +x benchmark_scripts/gobench.sh
+sudo chmod +x benchmark_scripts/fio_cephfs.sh
+sudo chmod +x benchmark_scripts/rados.sh
 
-#./benchmark_scripts/miniowarp.sh
+./benchmark_scripts/miniowarp.sh
 sudo ./benchmark_scripts/fio_rbd.sh > benchmark_results/fio_rbd.txt
-#./benchmark_scripts/gobench.sh
+./benchmark_scripts/gobench.sh
 sudo ./benchmark_scripts/fio_cephfs.sh > benchmark_results/fio_cephfs.txt
-#sudo ./benchmark_scripts/rados.sh > benchmark_results/rados.txt
+sudo ./benchmark_scripts/rados.sh > benchmark_results/rados.txt
