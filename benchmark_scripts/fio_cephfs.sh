cbbab23a9f1d5e2ca0e93d3a14214fc01f40d0c7
Cloud User <centos@bogomolov-ceph-4.novalocal>
2020-04-15 12:38:05 +0000

    Fixed similtaneous run of fio jobs, deleted hardcode in cephfs mountpath

--- benchmark_scripts/fio_cephfs.sh
+++ benchmark_scripts/fio_cephfs.sh
@@ -1,6 +1,4 @@
-source benchmark_configs/fio_cephfs.cfg
-mkdir cephfs_mountpoint
-sudo mount -t ceph ${monitors}:/ cephfs_mountpoint/
-mkdir cephfs_mountpoint/files
-
-fio benchmark_configs/fio_cephfs.ini
+fio benchmark_configs/fio_cephfs.ini --section="sequential_writes_bs4M_iodepth16"
+fio benchmark_configs/fio_cephfs.ini --section="sequential_reads_bs4M_iodepth16"
+fio benchmark_configs/fio_cephfs.ini --section="random_reads_bs4k_iodepth1"
+fio benchmark_configs/fio_cephfs.ini --section="deep_random_reads_bs4k_iodepth128"
--- benchmark_scripts/fio_rbd.sh
+++ benchmark_scripts/fio_rbd.sh
@@ -4,8 +4,16 @@ rbd feature disable block_device exclusive-lock object-map fast-diff deep-flatte
 sudo rbd map block_device --pool rbdbench --name client.admin
 sudo /sbin/mkfs.ext4 -m0 /dev/rbd/rbdbench/block_device
 
-fio benchmark_configs/fio_rbd.ini
+fio benchmark_configs/fio_rbd.ini --section="sequential_writes_bs4M_iodepth16"
+fio benchmark_configs/fio_rbd.ini --section="sequential_writes_bs4M_iodepth32"
+fio benchmark_configs/fio_rbd.ini --section="random_writes_bs4k_iodepth1"
+fio benchmark_configs/fio_rbd.ini --section="random_writes_bs4k_iodepth128"
+fio benchmark_configs/fio_rbd.ini --section="sequential_reads_bs4M_iodepth16"
+fio benchmark_configs/fio_rbd.ini --section="sequential_reads_bs4M_iodepth32"
+fio benchmark_configs/fio_rbd.ini --section="random_reads_bs4k_iodepth1"
+fio benchmark_configs/fio_rbd.ini --section="random_reads_bs4k_iodepth128"
 
 sudo rbd unmap block_device --pool rbdbench
 sudo rbd remove block_device --pool rbdbench
 ceph osd pool delete rbdbench rbdbench --yes-i-really-really-mean-it
+
